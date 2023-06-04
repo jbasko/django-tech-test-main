0f69957b7eb56c9c2aa5cacf1e54b9fb22402b4c
Jazeps Basko <jazeps.basko@gmail.com>
2023-06-04 15:04:19 +0300

    Author model

--- techtest/articles/models.py
+++ techtest/articles/models.py
@@ -7,3 +7,12 @@ class Article(models.Model):
     regions = models.ManyToManyField(
         'regions.Region', related_name='articles', blank=True
     )
+    author = models.ForeignKey(
+        'Author', related_name='articles', blank=True, null=True,
+        on_delete=models.SET_NULL,
+    )
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
